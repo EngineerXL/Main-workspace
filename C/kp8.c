b7d2fffd8dd9c82197e3368c01fa4239701da4dd
EngineerXL <8928632@gmail.com>
2020-04-27 11:27:11 +0300

    KP8.c update

--- C/kp8.c
+++ C/kp8.c
@@ -3,105 +3,104 @@
 
 struct list{
 	struct list* next;
-	int data;
+	int re, im;
 };
 
 typedef struct list list;
 
 int empty(list* lst){
-	return lst==NULL;
+	return (lst==NULL? 1:0);
 }
 
-list* find(list* lst, int p){
-	if(empty(lst)) return NULL;
-	while(p>1){
-		p--;
-		lst=lst->next;
-	}
-	return lst;
+list* next(list* lst){
+	if(empty(lst)) return lst;
+	else return lst->next;
 }
 
 int len(list* lst){
 	if(empty(lst)) return 0;
-	list* tmp=lst;
+	list* begin=lst;
 	int length=1;
-	while(lst->next!=tmp){
-		length++;
+	while(lst->next!=begin){
 		lst=lst->next;
+		length++;
 	}
 	return length;
 }
 
-void print(list* lst){
+list* append(list* it, int reval, int imval){
+	list* elem=malloc(sizeof(list));
+	elem->re=reval;
+	elem->im=imval;
+	if(empty(it)){
+		elem->next=elem;
+		return elem;
+	}else{
+		elem->next=it->next;
+		it->next=elem;
+		return it;
+	}
+}
+
+list* pop(list* it){
+	if(empty(it)) return NULL;
+	list* begin=it;
+	while(it->next!=begin) it=it->next;
+	if(it==begin) return NULL;
+	it->next=begin->next;
+	free(begin);
+	return it;
+}
+
+list* insert(list* lst, int p, int reval, int imval){
+	if(empty(lst)) return append(lst, reval, imval);
+	list* elem=malloc(sizeof(list));
+	elem->re=reval;
+	elem->im=imval;
+
+	list* begin=lst;
+	while(lst->next!=begin) lst=lst->next;
+	for(int i=0; i<p; i++) lst=lst->next;
+	lst=append(lst, reval, imval);
+	if(p==0) begin=lst->next;
+	return begin;
+}
+
+void print(list* lst, list* it){
 	if(empty(lst)) return;
-	list* tmp=lst;
-	while(lst->next!=tmp){
-		printf("%d ", lst->data);
+	list* begin=lst;
+	while(lst->next!=begin){
+		if(lst==it) printf("> %d+%di < ", lst->re, lst->im);
+		else printf("%d+%di ", lst->re, lst->im);
 		lst=lst->next;
 	}
-	printf("%d\n", lst->data);
+	if(lst==it) printf("> %d+%di <\n", lst->re, lst->im);
+	else printf("%d+%di\n", lst->re, lst->im);
 }
 
-void append(list** lst, int p, int x){
-	if(empty(*lst)){
-		*lst=malloc(sizeof(list));
-		(*lst)->data=x;
-		(*lst)->next=*lst;
-		return;
-	}
-	int p0=p;
-	list* tmp=*lst;
-	while((*lst)->next!=tmp){
-		*lst=(*lst)->next;
-	}
-	while(p>0){
-		p--;
-		*lst=(*lst)->next;
-	}
-	list* left=*lst;
-	list* right=(*lst)->next;
-	list* mid=malloc(sizeof(list));
-	mid->data=x;
-	left->next=mid;
-	mid->next=right;
-	if(p0==0) *lst=mid;
-	else *lst=tmp;
-}
-
-void pop(list** lst, int p){
-	if(len(*lst)<=1){
-		*lst=NULL;
-		return;
-	}
-	int p0=p;
-	list* tmp=*lst;
-	while((*lst)->next!=tmp){
-		*lst=(*lst)->next;
-	}
-	while(p>1){
-		p--;
-		*lst=(*lst)->next;
-	}
-	list* nnext=(*lst)->next->next;
-	free((*lst)->next);
-	(*lst)->next=nnext;
-	if(p0==1) *lst=(*lst)->next;
-	else *lst=tmp;
-}
-
-int main(){
-	int n, v, p;
-	scanf("%d", &n);
+signed main(){
 	list* lst=NULL;
-	for(int i=0; i<n; i++){
-		scanf("%d %d", &v, &p);
-		append(&lst, p, v);
-		//print(lst);
+	list* it=lst;
+	char c;
+	int p, reval, imval;
+	while(scanf("%c", &c)>0){
+		if(c==' ' || c=='\n' || c=='\t') continue;
+		if(c=='n') it=next(it);
+		if(c=='a'){
+			scanf("%d %d", &reval, &imval);
+			if(empty(lst)) lst=append(lst, reval, imval);
+			else it=append(it, reval, imval);
+		}
+		if(c=='l') printf("%d\n", len(lst));
+		if(c=='p'){
+			if(lst==it) lst=pop(lst);
+			else it=pop(it);
+		}
+		if(c=='i'){
+			scanf("%d %d %d", &p, &reval, &imval);
+			lst=insert(lst, p, reval, imval);
+		}
+		if(!empty(lst) && empty(it)) it=lst;
+		print(lst, it);
 	}
-	for(int i=0; i<n; i++){
-		scanf("%d", &p);
-		pop(&lst, p);
-		//print(lst);
-	}
-	print(lst);
 }
